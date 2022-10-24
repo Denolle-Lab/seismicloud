f958a75c40d6d825e83524ce95ccf901455cb48e
Yiyu Ni <niyiyu@uw.edu>
2022-10-24 12:04:35 -0700

    add argument parser

--- scripts/associate/association.py
+++ scripts/associate/association.py
@@ -7,7 +7,6 @@
 ###########################################
 
 import os
-import time
 import json
 import pickle
 import obspy
@@ -20,11 +19,14 @@ from gamma.utils import association
 from obspy.core.utcdatetime import UTCDateTime as utc
 
 parser = argparse.ArgumentParser(description="Phase association from PNW continuous phase detection.")
+parser.add_argument("-y", "--year", type=int, required=True)
+parser.add_argument("-c", "--config", required=True)
 
 args = parser.parse_args()
 
 ## load configure file
 fconfig = args.config
+year = args.year
 with open(fconfig, "r") as f:
     config = json.load(f)
 verbose = config["log"]["verbose"]
@@ -54,38 +56,37 @@ association_config["bfgs_bounds"] = (
 )
 
 
-for year in os.listdir(picks_path):
-    cs = []
-    for doy in os.listdir("/".join([picks_path, year])):
-        pick_df = []
-        for net in os.listdir("/".join([picks_path, year, doy])):
-            for sta in os.listdir("/".join([picks_path, year, doy, net])):
-                with open("/".join([picks_path, year, doy, net, sta]), 'rb') as f:
-                    picks = pickle.load(f)
-                if len(picks) >= 1:
-                    for p in picks:
-                        pick_df.append({
-                            "id": p.trace_id,
-                            "timestamp": p.peak_time.datetime,
-                            "prob": p.peak_value,
-                            "type": p.phase.lower(),
-                            "sod": p.peak_time.timestamp - utc(f"{year}{doy}").timestamp
-                        })
-        pick_df = pd.DataFrame(pick_df)
+cs = []
+for doy in os.listdir("/".join([picks_path, year])):
+    pick_df = []
+    for net in os.listdir("/".join([picks_path, year, doy])):
+        for sta in os.listdir("/".join([picks_path, year, doy, net])):
+            with open("/".join([picks_path, year, doy, net, sta]), 'rb') as f:
+                picks = pickle.load(f)
+            if len(picks) >= 1:
+                for p in picks:
+                    pick_df.append({
+                        "id": p.trace_id,
+                        "timestamp": p.peak_time.datetime,
+                        "prob": p.peak_value,
+                        "type": p.phase.lower(),
+                        "sod": p.peak_time.timestamp - utc(f"{year}{doy}").timestamp
+                    })
+    pick_df = pd.DataFrame(pick_df)
 
-        if len(pick_df)>0:
-            for h in range(24):
-                pick_df_hour = pick_df[(pick_df['sod'] > h*60*60) & (pick_df['sod'] < (h+1)*60*60)]
-                if len(pick_df_hour) > 1:
-                    c, assignments = association(pick_df_hour, station_df, association_config, method=association_config["method"])
-                    cs += c
-                else:
-                    pass
-    df_cats = pd.DataFrame(cs)
-    df_cats.sort_values("time", inplace = True)
-    cat = obspy.core.event.catalog.Catalog()
-    for ide, e in df_cats.iterrows():
-        event = obspy.core.event.event.Event()
-        event.origins.append(obspy.core.event.origin.Origin(time = utc(e['time'])))
-        cat.append(event)
-    cat.write(f"{catalog_path}/{year}.xml", format = "QUAKEML")
+    if len(pick_df)>0:
+        for h in range(24):
+            pick_df_hour = pick_df[(pick_df['sod'] > h*60*60) & (pick_df['sod'] < (h+1)*60*60)]
+            if len(pick_df_hour) > 1:
+                c, assignments = association(pick_df_hour, station_df, association_config, method=association_config["method"])
+                cs += c
+            else:
+                pass
+df_cats = pd.DataFrame(cs)
+df_cats.sort_values("time", inplace = True)
+cat = obspy.core.event.catalog.Catalog()
+for ide, e in df_cats.iterrows():
+    event = obspy.core.event.event.Event()
+    event.origins.append(obspy.core.event.origin.Origin(time = utc(e['time'])))
+    cat.append(event)
+cat.write(f"{catalog_path}/{year}.xml", format = "QUAKEML")
