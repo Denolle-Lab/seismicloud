323c8a2e8cc00ac5c9965a69fbe074d7183d4255
Yiyu Ni <niyiyu@uw.edu>
2022-10-24 12:22:38 -0700

    add clean-up scripts

---
+++ scripts/clean.py
@@ -0,0 +1,18 @@
+import os
+import json
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Phase detection on continuous mSEED data archive"
+)
+parser.add_argument("-c", "--config", required=True)
+args = parser.parse_args()
+
+fconfig = args.config
+with open(fconfig, "r") as f:
+    config = json.load(f)
+
+os.removedirs(config["workflow"]["jobs_path"])
+os.removedirs(config["workflow"]["picks_path"])
+os.removedirs(config["log"]["logs_path"])
+os.removedirs(config["workflow"]["catalog_path"])
